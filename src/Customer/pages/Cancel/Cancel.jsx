4a263e7991d3273007d7bd9c722bf71f8cc3ce0e
tthanhtung92 <55181588+tthanhtung92@users.noreply.github.com>
2022-10-20 01:15:54 +0700

    xong trang cancel

--- src/Customer/pages/Cancel/Cancel.jsx
+++ src/Customer/pages/Cancel/Cancel.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import "./Cancel.css"
 
 function Cancel() {
     return (
-        <>
-            <h1>Đã hủy lượt thanh toán</h1>
+        <div className="cancel-container">
+            <h1 className="cancel-notification">Đã hủy lượt thanh toán</h1>
             <Link to="/">
                 <button className="cancel-container__back">Trở về trang chủ</button>
             </Link>
-        </>
+        </div>
     );
 }
 export default Cancel;
